1307c54a5c61c3561a3308e4861bcfa272b2f7fc
AnyKeyer <slejv710@gmail.com>
2024-07-17 01:42:53 +0300

    FIx

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: '0.0.0.0', // Слушать на всех сетевых интерфейсах
     proxy: {
       '/api': {
         target: 'http://localhost:3000', // Адрес вашего Express сервера
